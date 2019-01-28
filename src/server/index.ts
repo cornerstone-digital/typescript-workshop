076ff11e8eb615ac56c8b9493c5a56b63fccd54f
Martin Egan <martingegan@gmail.com>
2019-01-28 16:59:58 +0000

    Updates

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,7 +2,6 @@ import express, { Application } from 'express'
 import { Server } from 'http'
 import SocketIO from 'socket.io'
 import { MongoClient } from 'mongodb'
-// import Mongoose from 'mongoose'
 
 const port: number = 3000
 const app: Application = express()
@@ -15,7 +14,7 @@ const url = 'mongodb://typescript:workshop1@ds159184.mlab.com:59184/typescript-w
 // Create a new MongoClient
 const client = new MongoClient(url)
 
-let members: string[] = []
+// let members: string[] = []
 
 const getMembers = () => {
   return [
@@ -33,22 +32,10 @@ client.connect(() => {
   io.on('connection', (socket: SocketIO.Socket): void => {
     const collection = app.get('db').collection('members')
     socket.on('USER:LOGIN', async (username) => {
-      // const exists = collection.find({ username })
-
-      // console.log('Exists', exists)
-
-      // if (!exists.length) {
-      //   console.log('Not existing')
-      //   await collection.insertOne({
-      //     username
-      //   })
-      // } else {
-      //   console.log('Already Exists')
-      // }
-
+      const members = getMembers()
       members.push(username)
       io.emit('USER:CONNECTED', `${username} has connected`)
-      io.emit('MEMBERS:UPDATED', getMembers())
+      io.emit('MEMBERS:UPDATED', members)
     })
   })
 })
